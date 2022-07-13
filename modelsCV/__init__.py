fe54ae668bac65d443b0cc78fe8effee47b3079c
rigelshysaj <63144832+rigelshysaj@users.noreply.github.com>
2022-07-13 13:45:52 +0200

    Update __init__.py

--- modelsCV/__init__.py
+++ modelsCV/__init__.py
@@ -1,18 +1,30 @@
-from .BasicModule import MyVggNet11
-from .BasicModule import MyVggNet13
-from .BasicModule import MyVggNet16
-from .BasicModule import MyVggNet19
-from .BasicModule import MyresNet18
-from .BasicModule import MyresNet34
-from .BasicModule import MyresNet50
-from .BasicModule import MyInceptionV3
-from .BasicModule import MydenseNet121
-from .BasicModule import MydenseNet161
-from .BasicModule import MydenseNet169
-from .BasicModule import MydenseNet201
-from .BasicModule import MyseNet1_0
-from .BasicModule import MyseNet1_1
-from .BasicModule import Myxception
-from .BasicModule import MydetNet59
-from .BasicModule import MynasNet
-from .BasicModule import MultiscaleresNet18
+import torch
+from torchvision import models
+import torch.nn as nn
+
+
+class MyresNet18(torch.nn.Module):
+    def __init__(self):
+        super(MyresNet18, self).__init__()
+        model = models.resnet18(pretrained = True)
+        self.resnet_lay=nn.Sequential(*list(model.children())[:-2])
+        self.conv1_lay = nn.Conv2d(512, 256, kernel_size = (1,1),stride=(1,1))
+        self.relu1_lay = nn.ReLU(inplace = True)
+        self.drop_lay = nn.Dropout2d(0.5)
+        self.global_average = nn.AdaptiveAvgPool2d((1,1))
+        self.fc_Linear_lay2 = nn.Linear(256,2)
+        
+
+    def load(self, path):
+       
+       self.load_state_dict(torch.load(path,map_location=torch.device('cpu')))
+
+    def forward(self, x):
+        x= self.resnet_lay(x)
+        x = self.conv1_lay(x)
+        x = self.relu1_lay(x)
+        x = self.drop_lay(x)
+        x= self.global_average(x)
+        x = x.view(x.size(0),-1)
+        x = self.fc_Linear_lay2 (x)
+        return x
